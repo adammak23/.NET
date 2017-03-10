7f0f33c73448ec5712be7eb7d9a2697ea4d11b0a
adammak2342 <adammak2342@o2.pl>
2017-03-10 07:40:00 +0100

    initial commit - class interface

---
+++ ConnectFour/ICzworki.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConnectFour
+{
+    public interface ICzworki
+    {
+        int CheckingDiagonal();
+        int CheckingHorizontal();
+        int CheckingVertical();
+        int index(int i, int j);
+        int tura(int row);
+        int TurnTimeout();
+        DateTime GetCurrentDate();
+    }
+}
